f06861c84da7637d65f8e8be32e4c36050a6f9b7
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-03-17 09:59:53 +0100

    done

--- src/components/blog/blog-item/BlogItem.jsx
+++ src/components/blog/blog-item/BlogItem.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import BlogAuthor from "../blog-author/BlogAuthor";
 import "./styles.css";
 const BlogItem = (props) => {
-  const { title, cover, author, _id } = props;
+  const { title, cover, authors, _id } = props;
   return (
     <Link to={`/blog/${_id}`} className="blog-link">
       <Card className="blog-card">
@@ -22,7 +22,7 @@ const BlogItem = (props) => {
           </Card.Title>
         </Card.Body>
         <Card.Footer>
-          <BlogAuthor {...author} />
+          <BlogAuthor {...authors[0]} />
         </Card.Footer>
       </Card>
     </Link>
